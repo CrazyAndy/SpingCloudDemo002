a9b832dbb51c1211b65b3d57e6c1607ea76ba40c
CrazyAndy <auden.sh@163.com>
2018-01-31 00:00:44 +0800

    主应用加入config server注解

--- trace-1/src/main/java/com/ntimes/trace1/api/TraceApi.java
+++ trace-1/src/main/java/com/ntimes/trace1/api/TraceApi.java
@@ -5,13 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import org.apache.log4j.Logger;
+
 @RestController
 public class TraceApi {
+    private final Logger logger = Logger.getLogger(getClass());
     @Autowired
     private RestTemplate restTemplate;
     @RequestMapping("/trace")
     public String trace(){
-        System.out.println("===call trace-1===");
+        logger.info("===call trace-1===");
         return restTemplate.getForEntity("http://trace-2/trace-2",String.class).getBody();
     }
 
--- trace-2/src/main/java/com/ntimes/trace2/api/TraceApi.java
+++ trace-2/src/main/java/com/ntimes/trace2/api/TraceApi.java
@@ -1,13 +1,18 @@
 package com.ntimes.trace2.api;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class TraceApi {
 
+    private final Logger logger = Logger.getLogger(getClass());
     @RequestMapping("/trace-2")
     public String trace(){
+        logger.info("===<call trace-2>===");
         return "trace-2";
     }
 }
