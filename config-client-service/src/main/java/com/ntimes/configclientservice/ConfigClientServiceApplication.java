8daa9877d81b120b19d2ece0d1015c0951c50b13
CrazyAndy <auden.sh@163.com>
2018-01-30 16:21:25 +0800

    config client

---
+++ config-client-service/src/main/java/com/ntimes/configclientservice/ConfigClientServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ntimes.configclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientServiceApplication.class, args);
+	}
+}
