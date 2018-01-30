5df256923f62ee31cb8fbce97d0c9c38a05a48a6
CrazyAndy <auden.sh@163.com>
2018-01-30 16:56:40 +0800

    config client

---
+++ config-client-service/src/main/java/com/ntimes/configclientservice/api/ConfigApi.java
@@ -0,0 +1,24 @@
+package com.ntimes.configclientservice.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:sunhu
+ * @Description:
+ * @Date Created in 2018/1/30 14:32
+ * @Modified By:
+ */
+@RefreshScope
+@RestController
+public class ConfigApi {
+    @Value("${from}")
+    private String from;
+    @RequestMapping("/from")
+    public String from(){
+        System.out.println(1);
+        return this.from;
+    }
+}
